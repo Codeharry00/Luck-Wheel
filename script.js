a028d86d4dadd70fbce55725c6cf57f7e84b2360
Codeharry00 <viethoang.ipro@gmail.com>
2024-06-16 22:28:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -116,3 +116,6 @@ spinButton.addEventListener('click', rotateWheel);
 
 // Tải các hình ảnh và vẽ vòng quay sau khi tải xong
 loadImages(drawWheel);
+
+// Tải các hình ảnh và vẽ vòng quay sau khi tải xong
+loadImages(drawWheel);
